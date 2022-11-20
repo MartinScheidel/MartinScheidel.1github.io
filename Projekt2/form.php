40492a371416871d8748ea20d6c33b2950fb1dca
MartinScheidel <slime.15.scheidel@gmail.com>
2022-11-20 13:53:33 +0100

    srklfbnklmydbnkjdh

---
+++ Projekt2/form.php
@@ -0,0 +1,28 @@
+$to = "slime.15.scheidel@gmail.com";
+$subject = "Subject"
+
+
+
+<body id="home">
+    <nav id="navbar">
+        <div id="logo">
+            <i class></i>
+            Auta
+            <span class="text-primary">Yes</span>
+        </div>
+    </nav>
+    <section id="success" class="bg-primary py-3 text-center">
+        <div class="container">
+            <h1 class="l-heading">Děkujeme za vaši zprávu.</h1>
+            <p class="lead">Odpovíme co nejdříve</p>
+            <a href="index.html">Zpátky na úvodní stránku</a>
+        </div>
+    </section>
+
+<footer id="main-footer" class="bg-dark text-center py-1">
+    <div class="container">
+        <p>Copyright why.sr.ro</p>
+    </div>
+</footer>
+
+</body>
